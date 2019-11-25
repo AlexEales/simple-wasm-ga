4c84550510840251df7ea07d945b57260209d1b7
alex.eales <alex.eales@potatolondon.com>
2019-11-25 14:27:59 +0000

    Added Generation Display update methods

--- src/index.ts
+++ src/index.ts
@@ -11,13 +11,35 @@ const POPULATION_SIZE_INPUT: HTMLInputElement = document.querySelector('#populat
 const MUTATION_RATE_DEFAULT = '0.2';
 const MUTATION_RATE_INPUT: HTMLInputElement = document.querySelector('#mutation-rate-input');
 
-const GENERATION_DISPLAY = document.querySelector('#generation-history-display');
-
 const RUN_BUTTON = document.querySelector('#run-button');
 const STOP_BUTTON = document.querySelector('#stop-button');
 const RESET_BUTTON = document.querySelector('#reset-button');
 
+const GENERATION_DISPLAY = document.querySelector('#generation-history-display');
+const DEFAULT_GENERATION_CARD = `
+<div class="inline-flex items-center w-full my-2 p-4 bg-white rounded-lg overflow-hidden border border-gray-300"
+        generation-display-card>
+    <div>
+        <h3 class="font-bold">No results</h3>
+        <p class="normal-case text-gray-500">Input your parameters and press start to begin!</p>
+    </div>
+</div>
+`;
+
 // FUNCTIONS
+const createGenerationCard = (generation, colour) => {
+    return `
+    <div class="inline-flex items-center w-full my-2 p-4 bg-white rounded-lg overflow-hidden border border-gray-300"
+            generation-display-card>
+        <div class="w-12 h-12 rounded mr-4" style="background-color: ${colour}"></div>
+        <div>
+            <h3 class="font-bold capitalize">generation ${generation}</h3>
+            <p class="uppercase text-gray-500">${colour}</p>
+        </div>
+    </div>
+    `;
+};
+
 const updateColourDisplay = () => {
     // Update the colour display when input changes.
     let colour = TARGET_COLOUR_INPUT.value;
@@ -25,6 +47,13 @@ const updateColourDisplay = () => {
         colour = '#' + colour;
     }
     TARGET_COLOUR_DISPLAY.style.backgroundColor = colour;
+};
+
+const clearGenerationDisplay = () => {
+    document.querySelectorAll('[generation-display-card]').forEach(
+        elem => elem.parentNode.removeChild(elem)
+    );
+    GENERATION_DISPLAY.insertAdjacentHTML('beforeend', DEFAULT_GENERATION_CARD);
 };
 
 // EVENT LISTENERS
@@ -55,6 +84,7 @@ RESET_BUTTON.addEventListener('click',  () => {
     updateColourDisplay();
     POPULATION_SIZE_INPUT.value = POPULATION_SIZE_DEFAULT;
     MUTATION_RATE_INPUT.value = MUTATION_RATE_DEFAULT;
+    clearGenerationDisplay();
 });
 
 console.log(module.hello());
